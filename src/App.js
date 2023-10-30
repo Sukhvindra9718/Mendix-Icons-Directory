2252dcfc2150a9d2e5a146d3582a088ab86b3b91
Sukhvindra9718 <sukhvindra.singh@resilientitservices.com>
2023-10-30 22:19:22 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import * as Icons from "@fortawesome/free-solid-svg-icons";
+import IconList from "./components/IconList";
+
+const iconList = Object.keys(Icons).filter(key => key !== "fas" && key !== "prefix").map(icon => Icons[icon]);
+library.add(...iconList);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IconList iconList={iconList}/>
     </div>
   );
 }
---
+++ src/components/IconList.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../App.css";
+
+function IconList({ iconList }) {
+  const [copyIconIndex, setCopyIconIndex] = React.useState(-1);
+  const [copyIcon, setCopyIcon] = React.useState(false);
+  const [searchIconList, setSearchIconList] = React.useState([]);
+  React.useEffect(() => {
+    if (copyIcon) {
+      setTimeout(() => {
+        console.log("1")
+        setCopyIcon(false);
+        clearTimeout();
+      }, 1000);
+    }
+  }, [copyIcon]);
+
+  const searchIcon = (e) => {
+    const searchValue = e.target.value;
+    const filteredIconList = iconList.filter((icon) => {
+      return icon.iconName.toLowerCase().includes(searchValue.toLowerCase());
+    });
+    setSearchIconList(filteredIconList);
+    console.log(filteredIconList);
+  }
+  React.useEffect(() => {
+    setSearchIconList(iconList);
+  }, [iconList]);
+  return (
+    <div className="container">
+      <h1>Icon List</h1>
+      <span>Click Icon Name to copy to clipboard</span>
+      <div className="search-container">
+        {/* Search box  */}
+        <input type="text" placeholder="Search icon" onChange={(e) => searchIcon(e)}/>
+
+      </div>
+      <div className="icon-container">
+        {searchIconList.length > 0 ? searchIconList.map((icon, index) => (
+          <div className="icon" key={index}>
+            <div className="icon-inner">
+              <div className="icon-wrapper">
+                <div className="icon-image">
+                  <FontAwesomeIcon icon={icon.iconName} color="#183153" />
+                </div>
+                <div className="icon-name">
+                  <p
+                    onClick={() => {
+                      setCopyIcon(true);
+                      setCopyIconIndex(index);
+                      navigator.clipboard.writeText(icon.iconName);
+                    }}
+                  >
+                    {icon.iconName}
+                  </p>
+                  <span
+                    className={`${copyIcon ? "copy-icon active" : "copy-icon"}`}
+                    onClick={() => {
+                      setCopyIcon(true);
+                      setCopyIconIndex(index);
+                      navigator.clipboard.writeText(icon.iconName);
+                    }}
+                  >
+                    <FontAwesomeIcon icon="copy" color="#183153" />
+                  </span>
+                  {copyIconIndex === index && (
+                    <span
+                      className={`${
+                        copyIcon ? "copy-message active" : "copy-message"
+                      }`}
+                    >
+                      Copied!
+                    </span>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        )): <p>No icon found</p>}
+      </div>
+    </div>
+  );
+}
+
+export default IconList;
